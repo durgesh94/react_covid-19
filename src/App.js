d4ce9bfb7232c9494eabd09bae26f2f0a7f8dc25
Durgesh Tambe <durgesh.tambe@varahitechnologies.com>
2020-04-24 13:42:53 +0530

    [24-Apr-2020] [Durgesh]: CountryPicker - Completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { fetchData } from "./api";
 
 class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    country: ''
   }
 
   async componentDidMount() {
@@ -14,13 +15,18 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
-        <Charts />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
       </div>
     );
   }
